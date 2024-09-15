2ad11121f24b8ae8f27afca7963a95a4ed025156
Cart Valderama <cartantonio2000@gmail.com>
2024-09-15 22:11:18 +0200

    fix bugs

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import ThemeSwitch from "../components/Theme-switch";
+import ThemeSwitch from "../components/ThemeSwitch";
 
 const text = "Welcome to my portfolio.";
 
