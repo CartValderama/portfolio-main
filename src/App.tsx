a975b57fa8fb3675f2519e4ab168dcf2b6d57491
Cart Valderama <cartantonio2000@gmail.com>
2024-10-30 17:07:56 +0100

    add design

--- src/App.tsx
+++ src/App.tsx
@@ -6,67 +6,12 @@ import { Toaster } from "react-hot-toast";
 import Body from "./layouts/Main";
 import ActiveSectionContextProvider from "./context/active-section-context";
 import { Analytics } from "@vercel/analytics/react";
-import { motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
 
 export default function App() {
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"],
-  });
-
-  const planeY = useTransform(scrollYProgress, [1, 0], ["-100%", "10%"]);
-  const planeX = useTransform(scrollYProgress, [0, 1], ["-100%", "150%"]);
-  const planeScale = useTransform(scrollYProgress, [1, 0], ["0%", "150%"]);
-
-  const asteroidY = useTransform(scrollYProgress, [1, 0], ["0%", "100%"]);
-  const asteroidX = useTransform(scrollYProgress, [1, 0], ["-20%", "100%"]);
-  const asteroidScale = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
-
   return (
     <ThemeContextProvider>
       <ActiveSectionContextProvider>
-        <div
-          ref={ref}
-          className="max-w-4xl sm:mt-2 gap-6 flex m-auto flex-col text-primary dark:text-opacity-85 dark:text-[#fcf9ec] relative"
-        >
-          {/* plane */}
-          <motion.img
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 2 }}
-            style={{ y: planeY, x: planeX, scale: planeScale }}
-            aria-hidden
-            src="https://i.imgur.com/zebyBvC.png"
-            className="-z-10 w-[16rem] -left-14 top-0 fixed dark:hidden"
-            alt="pilot's plane"
-          />
-          {/* asteroid */}
-          <motion.img
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 2 }}
-            style={{ y: asteroidY, x: asteroidX, scale: asteroidScale }}
-            aria-hidden
-            src="https://i.imgur.com/zzfa7JK.png"
-            className="w-[16rem] 2xl:w-[24rem] -right-14 bottom-0 fixed dark:hidden"
-            alt="little prince's asteroid"
-          />
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 0.3, scale: 1 }}
-            transition={{ duration: 1.5 }}
-            aria-hidden
-            className="fixed -z-30 bg-red-50 left-[8rem] rounded-full w-[40rem] h-[40rem] blur-3xl dark:bg-slate-100 dark:hidden hidden lg:block"
-          ></motion.div>
-          <motion.div
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 0.3, scale: 1 }}
-            transition={{ duration: 1.5 }}
-            aria-hidden
-            className="fixed -z-30 bg-amber-50 right-[8rem] top-[16rem] rounded-full w-[40rem] h-[40rem] blur-3xl dark:bg-slate-100 dark:hidden hidden lg:block"
-          ></motion.div>
+        <div className="max-w-4xl sm:mt-2 gap-6 flex m-auto flex-col text-primary dark:text-opacity-85 dark:text-[#fcf9ec] relative">
           <Toaster
             position="bottom-center"
             containerClassName="mb-32 md:mb-0"
--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -4,12 +4,64 @@ import Introduction from "../components/Introduction";
 import Education from "@/components/Education";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+import { FaSun } from "react-icons/fa6";
+import { FaMoon } from "react-icons/fa";
+import { useTheme } from "@/utils/useContext";
 
 const queryClient = new QueryClient();
 
 export default function Main() {
+  const { theme } = useTheme();
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const luminariesY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const luminariesX = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const luminariesScale = useTransform(
+    scrollYProgress,
+    [0, 0.5],
+    ["100%", "200%"]
+  );
+  const luminariesRotation = useTransform(scrollYProgress, [0, 1], [0, 120]);
+
   return (
-    <main className="flex flex-col gap-6 ">
+    <main ref={ref} className="flex flex-col gap-6 ">
+      {/* sun */}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 0.6 }}
+        transition={{ duration: 1 }}
+        style={{
+          y: luminariesY,
+          x: luminariesX,
+          scale: luminariesScale,
+          rotateZ: theme === "dark" ? 0 : luminariesRotation,
+        }}
+        aria-hidden
+        className="-z-10 left-2 top-3 fixed hidden xl:block "
+      >
+        <FaMoon className="text-4xl text-sky-900 backdrop-blur-sm hidden dark:block" />
+        <FaSun className="text-4xl text-yellow-400 backdrop-blur-sm dark:hidden" />
+      </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 0.3 }}
+        transition={{ duration: 1 }}
+        style={{
+          y: luminariesY,
+          x: luminariesX,
+          scale: luminariesScale,
+        }}
+        aria-hidden
+        className="fixed -z-30 bg-amber-200 -left-[7.5rem] -top-[7.5rem] rounded-full w-[9rem] h-[9rem] blur-2xl dark:bg-blue-900 dark:-left-[12rem] dark:-top-[12rem] hidden lg:block"
+      ></motion.div>
+
       <Introduction />
       <Skills />
       <Education />
